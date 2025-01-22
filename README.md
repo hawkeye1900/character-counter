9efbe6744066b3b4751ba7da42c315bbfdbdf8e1
hawkeye <173827852+hawkeye1900@users.noreply.github.com>
2025-01-22 15:54:09 +0000

    reset button added to clear input field and reset count. README file added

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project Name
+
+Character Counter
+
+## Description
+
+Takes text, either copied or written into the input box and counts the number of characters using a reg ex.
+
+Reset button allows the input box to be cleared and count returned to zero
--- scipt.js
+++ scipt.js
@@ -1,11 +1,17 @@
 const result = document.getElementById("result");
-const runCountBtn = document.querySelector(".countBtn");
+const runCountBtn = document.querySelector(".Btn");
+const resetBtn = document.querySelector(".resetBtn");
 
 runCountBtn.addEventListener("click", count);
+resetBtn.addEventListener("click", reset);
 
 function count() {
   let text = document.getElementById("inputText").value;
-  console.log(text);
   let charCount = text.match(/./g)?.length || 0;
   result.textContent = charCount;
+}
+
+function reset() {
+  document.getElementById("inputText").value = "";
+  result.textContent = 0;
 }
